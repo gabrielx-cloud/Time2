ce62e30817b3043f406f2c8f814592f131373316
gabrielx-cloud <gabriel.x@edu.senai.br>
2026-03-30 11:06:59 -0300

    Add files via upload

---
+++ Login/java.js
@@ -0,0 +1,7 @@
+function logar() {
+ Swal.fire({
+        title: "Sua conta foi feita com sucesso",
+        text: "Obrigado pela participação",
+        icon: "success"
+        });
+    }
---
+++ Login/javascript.js
@@ -0,0 +1,7 @@
+function logar() {
+ Swal.fire({
+        title: "Seus dados foram enviados",
+        text: "Obrigado pela cooperação",
+        icon: "success"
+        });
+    }
